6d7bace1c117fcb5be28d05c9724244a75669869
vaddeswathi <swathirose91@gmail.com>
2018-06-20 18:39:34 +0200

    addednew project second_project2j

---
+++ Second_Project2J/src/com/hdfc/loans/businessloans/BusinessClass.java
@@ -0,0 +1,37 @@
+package com.hdfc.loans.businessloans;
+
+public class BusinessClass {
+	public void medicine()
+	{
+		int p=10;
+		int c=20;
+		int m=0;
+		m=c+p;
+		System.out.println("p,c,m are medicines");
+	}
+	public void grocery()
+	{
+		int a=50;
+		int b=60;
+		int c=80;
+		int d=0;
+		d=a+b+c;
+		System.out.println("d is addition of a,b,c");
+		
+	}
+	public void fruits()
+	{
+		int a=5;
+		int b=6;
+		int g=7;
+		int w=0;
+		 w=g+a+b;
+		 System.out.println("w is add of a,b,g");
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("business loans");
+
+	}
+
+}
