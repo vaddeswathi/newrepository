8f10587e453d00ae11a3befd1b41e435c5924a3f
vaddeswathi <swathirose91@gmail.com>
2018-06-20 19:07:02 +0200

    entered another space

--- Second_Project2J/src/com/hdfc/loans/businessloans/NewRClass.java
+++ Second_Project2J/src/com/hdfc/loans/businessloans/NewRClass.java
@@ -3,6 +3,7 @@ package com.hdfc.loans.businessloans;
 public class NewRClass {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		
 System.out.println("this is the new repository class");
